643a0ad6bf6d3a72b0ada3fd01b74f3ad3ce6a20
Prabhat <prabhatzadepatil@gmail.com>
2025-08-07 15:29:59 +0530

    Code updated in app.py file

--- src/recommender.py
+++ src/recommender.py
@@ -1,7 +1,7 @@
 from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
 from src.prompt_template import get_anime_prompt
-
+ 
 class AnimeRecommender:
     def __init__(self,retriever,api_key:str,model_name:str):
         self.llm = ChatGroq(api_key=api_key,model=model_name,temperature=0)
